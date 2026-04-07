56c4eff3ab700ea41e92330fc5ed1f2b553387a5
matteodegobbi <matteodegobbi2001@gmail.com>
2026-04-07 14:31:22 +0200

    personal config changes

--- init.lua
+++ init.lua
@@ -772,12 +772,6 @@ require('lazy').setup({
         nerd_font_variant = 'mono',
       },
 
-      completion = {
-        -- By default, you may press `<c-space>` to show the documentation.
-        -- Optionally, set `auto_show = true` to show the documentation after a delay.
-        documentation = { auto_show = false, auto_show_delay_ms = 500 },
-      },
-
       sources = {
         default = { 'lsp', 'path', 'snippets' },
       },
